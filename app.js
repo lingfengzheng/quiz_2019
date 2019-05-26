109562810fd59705c3ec61fb33cfeae9f2e7f4b4
Lingfeng Zeng <lingfeng.zheng@alumnos.upm.es>
2019-05-26 19:47:13 +0200

    Step8

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var partials = require('express-partials')
+var logger = require('morgan');
+var partials = require('express-partials');
+var methodOverride = require('method-override');
 
-var index = require('./routes/index');
-var author = require('./routes/author');
-
+var indexRouter = require('./routes/index');
 
 var app = express();
 
@@ -16,23 +15,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'fav.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method', {methods: ["POST", "GET"]}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
 
-app.use('/', index);
-app.use('/', author);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
 });
 
 // error handler
