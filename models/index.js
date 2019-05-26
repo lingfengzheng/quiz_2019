b5bb8e9850bbe354beff65f8c17eef83b3953e76
Lingfeng Zeng <lingfeng.zheng@alumnos.upm.es>
2019-05-26 23:56:28 +0200

    Step Flash Message

--- models/index.js
+++ models/index.js
@@ -15,7 +15,8 @@ const sequelize = new Sequelize(url);
 // Import the definition of the Quiz Table from quiz.js
 sequelize.import(path.join(__dirname, 'quiz'));
 
-
+// Session
+sequelize.import(path.join(__dirname,'session'));
 
 // Create tables
 sequelize.sync()
